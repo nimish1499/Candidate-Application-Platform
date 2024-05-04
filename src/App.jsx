54b0937f4291169f01a2426c017ad64f0c9c7465
Nimish Bahuguna <55277101+nimish1499@users.noreply.github.com>
2024-05-04 18:05:22 +0000

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import "./styles.css";
+import { jobData } from "./data/jobData";
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  console.log(jobData);
+  return <div className="App">Candidate Application Form</div>;
 }
